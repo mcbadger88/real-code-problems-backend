77e0e1fe91cf6bd8e85546239d740f1019cf7994
Emma <emmacullen8@gmail.com>
2020-01-21 13:00:45 +1100

    initial efforts

--- controllers/attemptsController.js
+++ controllers/attemptsController.js
@@ -1,6 +1,8 @@
 const Attempt = require('../models/Attempt')
 const AWS = require('aws-sdk');
 const uuidv1 = require('uuid/v1');
+var FormData = require('form-data');
+const axios = require('axios')
 
 // returns a list of attempts, with populated candidate or challenge data, depending on the idtype.
 // http://localhost:5000/candidates/5df865457947111ec1a81cad/attempts
@@ -54,22 +56,89 @@ const create = async(req, res) => {
 
 const update = async(req, res) => {
     console.log("in attempt update")
-    console.log(req)
+    console.log(`req.body 1 ${JSON.stringify(req.body)}`)
+    console.log(`req ${JSON.stringify(req)}`)
+    // console.log(req.files)
     
-    //look up the attempt by the attempt uuid ModelObj.findOne({ 'uuid': IdValue}).exec(callback);
+    // look up the attempt by the attempt uuid 
     //send the req.body to michaels API "https://stg-real-code-runner.herokuapp.com/submissions"
     // if successful, save attempt.status to "SUBMITTED"
     // return the results of Michaels API call 
+    const updatedAttempt = await Attempt.findOne({ 'uuid': req.body.external_user_identifier})
+    console.log(`found attempt ${updatedAttempt}`)
+
+    const data = req.body
+
+    // send req.body to michaels server
+    //send the req.body to michaels API "https://stg-real-code-runner.herokuapp.com/submissions"
+    var bodyFormData = new FormData();
+    console.log("after new form data")
+    for (key in data){
+        console.log(`saving key ${JSON.stringify(key)} ${JSON.stringify(data[key])}`)
+        bodyFormData.append(JSON.stringify(key), JSON.stringify(data[key]));
+    }
+
     try{
-    //    const updatedAttempt = await Attempt.findOne({ 'uuid': IdValue})findById(req.params.attemptid);
-       const data = req.body;
-       for (key in data){
-           updatedAttempt[key] = data[key]
-           await Attempt.updateOne({_id: updatedAttempt._id}, updatedAttempt);
-       }
-       res.send(updatedAttempt)
+
+        // bodyFormData.append('userName', 'Fred');
+        // bodyFormData.append('file', imageFile); 
+        // user_token: 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImE4MGE5MGEwLTAwZTctNDQ0Yy1hMDgzLWY3ZWI5YjU4ZjgxZiIsImV4cCI6MTU4NDQ5NjQyNX0.ekjDzZNUjFsTS3dmZCKOWPyY29_uJM7rMC6P_ipAu9M',
+        // submission: [Object: null prototype] {
+        //   challenge_id: 'd618a0cf-80f5-4368-acda-bd6f8f296ba0',
+        //   external_user_identifier: '764aa5d0-3b1f-11ea-acfb-439a69150d3b',
+        //   url: '',
+        //   text: '<h1>hello</h1>'
+        // }
+        console.log("after body form data")
+        console.log(`body form data ${bodyFormData}`)
+
+        let response = await axios({
+            method: 'post',
+            url: 'https://stg-real-code-runner.herokuapp.com/submissions',
+            data: bodyFormData,
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Cookie': "HnKFn0EwUl%2FPhBDmb6sGN4TX645oU7q7qwIUkQ3orUZdMSQNXapV4VI8jauY6Dqc%2BxZWfMlxYY9fbCOZzF6ZSqagMioHmqYWwnSS0dZxFFjxo00eAT9V9gqiKjgsNkoqgzMrBRskIQ7pz6ruy5zpeftIrsPc7Cdw8x%2B%2FIngEXZz1tnLgnXpUMAQQWYioTzdBPCLTOA772MNUyG%2Fsbeg8yCxlJgWiJQDOIb%2BRpLVozTIifYwaTPmW1Qnj1%2FBDWSjTuqIcUoZHtU%2FomEMPraWlHhbWHs25d7NuVjBkXrvPYibkITB8Ws8qtC8A0rR14qdbtejBQT%2FIzcziyh5b2zsWcvlp4R26ojqoucQIcIb6541w1DriQ3dKXktiqpkVAp0kXQ%2F7fo7jornVCenQ81B9B8uSGAFXchiBO17rr4izbojsbQfKHzn595qFgMWHOTPf%2Bl0hdZF75BKPWA6IP7ul0nIGykdq--neNSH66Gh3JzVez%2F--Vqqrg1Eo3D%2F7uYf5VFVgvw%3D%3D"
+             }
+        })
+
+        console.log(`response ${response}`)
+
+
+
+            
+        // if () {
+        //     updatedAttempt[status] = "RESULTS PENDING"
+        //     await Attempt.updateOne({_id: updatedAttempt._id}, updatedAttempt);
+
+        //     res.send(updatedAttempt)
+        // } else {
+
+        // }
+        res.send(updatedAttempt)
     } catch(error){
-        res.status(404).send(error)
+        if (error.response) {
+            /*
+             * The request was made and the server responded with a
+             * status code that falls out of the range of 2xx
+             */
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            res.status(404).send(error.response)
+        } else if (error.request) {
+            /*
+             * The request was made but no response was received, `error.request`
+             * is an instance of XMLHttpRequest in the browser and an instance
+             * of http.ClientRequest in Node.js
+             */
+            console.log(error.request);
+            res.status(404).send(error.request)
+    } else {
+            // Something happened in setting up the request and triggered an Error
+            console.log('Error', error.message);
+            res.status(404).send(error.message)
+    }
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require("body-parser");
+
+const multer = require('multer')
+//Multer middleware
+const multerMiddleware = multer({ storage: multer.memoryStorage() }).single('file')
 
 // Importing all route files
 const attempts = require('./attempts');
@@ -12,9 +15,9 @@ const seed = require('./seed');
 
 // Defining paths for each route file.
 router.use('/candidates', express.json(), candidates);
-router.use('/challenges', bodyParser.json(), express.json(), challenges)
+router.use('/challenges', express.json(), challenges)
 router.use('/challenges/:id/features', features)
-router.use('/:idtype/:id/attempts', express.json(), attempts)
+router.use('/:idtype/:id/attempts', multerMiddleware, express.json(), attempts)
 router.use('/user', users)
 
 module.exports = router;
